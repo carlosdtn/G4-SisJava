7118d797bbe878df4fd97cd31e4b189469aa9d4d
Jorge Marin <jm.dev97@gmail.com>
2023-05-10 01:30:41 -0500

    Refacturación de métodos en LoginDAO
    - Refactoriza métodos (3) dentro de clase LoginDAO.
    - Modifica la clave a la base de datos en clase Conexión.

--- src/Modelo/LoginDAO.java
+++ src/Modelo/LoginDAO.java
@@ -1,4 +1,3 @@
-
 package Modelo;
 
 import java.sql.Connection;
@@ -9,55 +8,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LoginDAO {
+
     Connection con;
     PreparedStatement ps;
     ResultSet rs;
     Conexion cn = new Conexion();
-    
+
     /*
     MODIFICACIÓN
     @JorgeMarin
-    Objetivo: mejorar la eficiencia de la método "log()" se propone.
-    */
-    public login log(String correo, String pass){
-        /*
-        La variable 'l' se inicializa en null para evitar la creación 
-        innecesaria de una conexión que pueda consumir recursos del 
-        sistema en caso de que la ejecución del método falle por algún motivo.
-        login l = NULL;
-        */
-        login l = new login();
+    - Agregar sentencia "finally" como buena práctica de programación
+    - Cerrar la conexión con la base de datos
+    - Liberar la memoria de los objetos rs, ps
+    - Utiliza constructor de clase login
+     */
+    public login log(String correo, String pass) {
+        login l = null;
         String sql = "SELECT * FROM usuarios WHERE correo = ? AND pass = ?";
-        try {
-            con = cn.getConnection();
-            ps = con.prepareStatement(sql);
+        try ( Connection con = cn.getConnection();  PreparedStatement ps = con.prepareStatement(sql)) {
             ps.setString(1, correo);
             ps.setString(2, pass);
-            rs= ps.executeQuery();
-            if (rs.next()) {
-                /*
-                Si existe un registro, recien se debe inicializar
-                el objeto login.
-                l = new login();
-                */
-                l.setId(rs.getInt("id"));
-                l.setNombre(rs.getString("nombre"));
-                l.setCorreo(rs.getString("correo"));
-                l.setPass(rs.getString("pass"));
-                l.setRol(rs.getString("rol"));
-                
+            try ( ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    l = new login(rs.getInt("id"), rs.getString("nombre"),
+                            rs.getString("correo"), rs.getString("pass"),
+                            rs.getString("rol"));
+                }
             }
         } catch (SQLException e) {
             System.out.println(e.toString());
-        }
-        /*
-        Se recomienda agregar una cláusula 'finally' al usar sentencias 
-        try-catch. Esto garantiza que se ejecute siempre cierto código, 
-        independientemente de si se lanzó una excepción o no, lo que puede 
-        ser útil para liberar recursos o realizar otras tareas de limpieza.
-        
-        finally {
-        
+        } finally {
             try {
                 if (rs != null) {
                     rs.close();
@@ -66,29 +46,23 @@ public class LoginDAO {
                     ps.close();
                 }
                 if (con != null) {
-                    con.close(); // cerrar la conexión
+                    con.close();
                 }
             } catch (SQLException e) {
                 System.out.println(e.toString());
             }
         }
-        
-        Es importante cerrar la conexión a la base de datos al finalizar 
-        las acciones. De esta forma, se evita consumir recursos innecesarios 
-        en el servidor y se garantiza una ejecución eficiente y segura 
-        del programa
-        */
         return l;
     }
-    
+
     /*
     MODIFICACIÓN
     @Jorge Marin
     - Agregar sentencia "finally" como buena práctica de programación
     - Cerrar la conexión con la base de datos
     - Liberar la memoria de los objetos rs, ps.
-    */
-    public boolean Registrar(login reg){
+     */
+    public boolean Registrar(login reg) {
         String sql = "INSERT INTO usuarios (nombre, correo, pass, rol) VALUES (?,?,?,?)";
         try {
             con = cn.getConnection();
@@ -102,40 +76,61 @@ public class LoginDAO {
         } catch (SQLException e) {
             System.out.println(e.toString());
             return false;
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (con != null) {
+                    con.close(); // cerrar la conexión
+                }
+            } catch (SQLException e) {
+                System.out.println(e.toString());
+            }
         }
     }
-    
+
+
     /*
     MODIFICACIÓN
     @Jorge Marin
-    No es recomendable traer todas las tuplas de datos, consume recursos del
-    sistema y relentiza el programa.
-    
-    Se propone:
-    - Evitar listar todos los usuarios
-    - Consultar solo por clave de usuario específico
     - Agregar sentencia "finally" como buena práctica de programación
     - Cerrar la conexión con la base de datos
     - Liberar la memoria de los objetos rs, ps.
      */
-    public List ListarUsuarios(){
-       List<login> Lista = new ArrayList();
-       String sql = "SELECT * FROM usuarios";
-       try {
-           con = cn.getConnection();
-           ps = con.prepareStatement(sql);
-           rs = ps.executeQuery();
-           while (rs.next()) {               
-               login lg = new login();
-               lg.setId(rs.getInt("id"));
-               lg.setNombre(rs.getString("nombre"));
-               lg.setCorreo(rs.getString("correo"));
-               lg.setRol(rs.getString("rol"));
-               Lista.add(lg);
-           }
-       } catch (SQLException e) {
-           System.out.println(e.toString());
-       }
-       return Lista;
-   }
+    public List<login> ListarUsuarios() {
+        List<login> Lista = new ArrayList<>();
+        String sql = "SELECT * FROM usuarios";
+        try {
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                login lg = new login();
+                lg.setId(rs.getInt("id"));
+                lg.setNombre(rs.getString("nombre"));
+                lg.setCorreo(rs.getString("correo"));
+                lg.setRol(rs.getString("rol"));
+                Lista.add(lg);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (ps != null) {
+                    ps.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.toString());
+            }
+        }
+        return Lista;
+    }
+
 }
